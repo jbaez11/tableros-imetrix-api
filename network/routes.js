b44cd25d3431f7922749dd7c77da9b76df259832
Jonathan Baez <jbaez9910@gmail.com>
2021-08-23 11:59:39 -0500

    subir nuevos cambios

--- network/routes.js
+++ network/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const basekeywords = require('../components/baseKeywords/network');
 const agents = require('../components/agents/network');
-const basephrases = require('../components/basePhrases/network');
+//const basephrases = require('../components/basePhrases/network');
 const agentsaudit = require('../components/agentsAudit/network');
-const agentsauditphrases = require('../components/agentsAuditPhrases/network');
+//const agentsauditphrases = require('../components/agentsAuditPhrases/network');
 const consumption = require('../components/consumption/network');
 const keywords = require('../components/keywords/network');
-const phrases = require('../components/phrases/network');
+//const phrases = require('../components/phrases/network');
 const scores = require('../components/scorebykeywords/network');
 const scoringkeywords = require('../components/scoringKeywords/network');
 const auditconversations = require('../components/auditconversations/network');
@@ -15,25 +15,23 @@ const stadisticsofkeywords = require('../components/stadisticsofkeywords/network
 const basescore = require('../components/basescore/network');
 const basescores = require('../components/basescores/network');
 const auditkeywordstatistics = require('../components/auditkeywordstatistics/network');
-//
-const categories = require('../components/categorias/network');
+
+const basecategories = require('../components/categorias/network');
 const modules = require('../components/modulos/network');
 const clusters = require('../components/clusters/network');
 const statisticsofclusters = require('../components/statisticsofclusters/network');
 
 
 
-
-
 const routes = function(server){
     server.use(`/:bd/basekeywords`, basekeywords);
     server.use('/:bd/baseagents', agents);
-    server.use('/:bd/basephrases', basephrases);
+    //server.use('/:bd/basephrases', basephrases);
     server.use('/:bd/auditkeywords', agentsaudit);
-    server.use('/:bd/auditphrases', agentsauditphrases);
+    //server.use('/:bd/auditphrases', agentsauditphrases);
     server.use('/:bd/consumptions', consumption);
-    server.use('/:bd/keywords', keywords); 
-    server.use('/:bd/phrases', phrases);
+    server.use('/:bd/keywords', keywords);
+    //server.use('/:bd/phrases', phrases);
     server.use('/:bd/auditscoringkeywords', scores);
     server.use('/:bd/scoringkeywords', scoringkeywords);
     server.use('/:bd/auditconversations', auditconversations);
@@ -42,10 +40,11 @@ const routes = function(server){
     server.use('/:bd/basescore',basescore);
     server.use('/:bd/basescoresp',basescores);
     server.use('/:bd/auditkeywordstatistics',auditkeywordstatistics);
-    server.use('/:bd/categories',categories);
+    server.use('/:bd/basecategories',basecategories);
     server.use('/:bd/modules',modules);
     server.use('/:bd/clusters',clusters);
     server.use('/:bd/statisticsofclusters',statisticsofclusters);
+    
     
 }
 
